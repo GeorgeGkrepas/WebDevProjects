f6a2698393f76c12c4a1cf2e7f47cd5831fabac6
GeorgeGkrepas <geo.gkrepas@gmail.com>
2026-01-24 17:11:14 +0200

    Update Navbar.tsx

--- RestaurantReviewApp/src/components/Navbar.tsx
+++ RestaurantReviewApp/src/components/Navbar.tsx
@@ -16,19 +16,19 @@ export const Navbar = ({ openModal, setOpenModal }: NavbarProps) => {
   return (
     <>
       <header className="bg-gray-500 sticky top-0 z-20 mx-auto flex w-full items-center justify-between border-b border-gray-500 p-4">
-          <div className="text-2xl font-bold text-white">
-            Restaurant Reviews
-          </div>
-          <div className="text-base flex gap-4">
-            {currentUser === null && <Fragment>
-              <NavLink linkName="Log In" onClick={() => openModal === "login" ? setOpenModal(null) : setOpenModal("login")} />
-              <NavLink linkName="Sign Up" onClick={() => openModal === "signup" ? setOpenModal(null) : setOpenModal("signup")} />
-            </Fragment>}
-            {currentUser !== null && <Fragment>
-              <span className="text-white">Hello, {currentUser?.username}! {currentUser.emailVerified ? " ✅" : " ❌"}</span>
-              <NavLink linkName="Log Out" onClick={() => {logoutUser()}} />
-            </Fragment>}
-          </div>
+        <div className="text-2xl font-bold text-white">
+          Restaurant Reviews
+        </div>
+        <div className="text-base flex gap-4">
+          {currentUser === null && <Fragment>
+            <NavLink linkName="Log In" onClick={() => openModal === "login" ? setOpenModal(null) : setOpenModal("login")} />
+            <NavLink linkName="Sign Up" onClick={() => openModal === "signup" ? setOpenModal(null) : setOpenModal("signup")} />
+          </Fragment>}
+          {currentUser !== null && <Fragment>
+            <span className="text-white">Hello, {currentUser?.username}! {currentUser.emailVerified ? " ✅" : " ❌"}</span>
+            <NavLink linkName="Log Out" onClick={() => {logoutUser()}} />
+          </Fragment>}
+        </div>
       </header>
     </>
   )
