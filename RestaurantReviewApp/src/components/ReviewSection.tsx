ce23d8b3cafc14c6c63031f20df720cc72aa0379
GeorgeGkrepas <geo.gkrepas@gmail.com>
2026-02-08 17:33:50 +0200

    Minor tweak

--- RestaurantReviewApp/src/components/ReviewSection.tsx
+++ RestaurantReviewApp/src/components/ReviewSection.tsx
@@ -3,7 +3,8 @@ import { ReviewList } from './ReviewList';
 
 export const ReviewSection = () => {
   return (
-    <div className="max-w-5xl mx-auto p-4">
+    <div className="max-w-6xl mx-auto p-4">
+        <h1 className="text-3xl font-bold mb-5 text-center w-full underline">Reviews Section</h1>
         <AddReview />
         <ReviewList />
     </div>
