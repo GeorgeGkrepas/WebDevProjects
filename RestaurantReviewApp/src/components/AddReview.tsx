b894dd88fa4b23e6ca85d8e5260c4042cef0eb01
GeorgeGkrepas <geo.gkrepas@gmail.com>
2026-02-16 14:43:51 +0200

    Small changes

--- RestaurantReviewApp/src/components/AddReview.tsx
+++ RestaurantReviewApp/src/components/AddReview.tsx
@@ -9,11 +9,12 @@ import { addReview } from "./firebase"
         { value: "🍔Burgers", label: "🍔Burgers" },
         { value: "🍜Noodles", label: "🍜Noodles" },
         { value: "🍣Sushi", label: "🍣Sushi" },
+        { value: "🏛️Greek", label: "🏛️Greek" },
         { value: "🍰Desserts", label: "🍰Desserts" },
         { value: "🍗Fried Chicken", label: "🍗Fried Chicken" },
         { value: "🥪Sandwiches", label: "🥪Sandwiches" },
         { value: "🥗Salads", label: "🥗Salads" },
-        { value: "🍖BBQ", label: "🍖BBQ" },
+        { value: "🍖Grill/BBQ", label: "🍖Grill/BBQ" },
         { value: "🌮Tacos", label: "🌮Tacos" },
         { value: "☕Coffee", label: "☕Coffee" },
         { value: "🍹Drinks", label: "🍹Drinks" }
@@ -51,18 +52,18 @@ export const AddReview = () => {
 
     return (
         <div className="mx-auto p-4 outline-2 outline-gray-900 mb-6 rounded-xl">
-            <form onSubmit={handleSubmit} className="flex flex-col md:flex-row items-center gap-4">
+            <form onSubmit={handleSubmit} className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
                 <input
                     type="text"
                     placeholder="Restaurant Name"
                     value={restaurantName}
                     maxLength={25}
                     onChange={(e) => setRestaurantName(e.target.value)}
-                    className="p-2 rounded text-black outline"
+                    className="p-2 w-full rounded text-black outline"
                     required
                 />
 
-                <select className="p-2 rounded text-black outline" value={rating} onChange={(e) => setRating(e.target.value)} required>
+                <select className="p-2 w-full rounded text-black outline" value={rating} onChange={(e) => setRating(e.target.value)} required>
                     <option value="" disabled>Select Rating</option>
                     <option value="1">1⭐</option>
                     <option value="2">2⭐</option>
@@ -80,7 +81,7 @@ export const AddReview = () => {
                         value={category}
                         onChange={(selected) => setCategory(selected as any[])}
                         placeholder="Select Categories"
-                        className="text-black bg-gray-300 rounded"
+                        className="w-full text-black bg-gray-300 rounded"
                         classNamePrefix="react-select"
                     />
                 </div>
@@ -91,7 +92,7 @@ export const AddReview = () => {
                     value={favoriteDishes}
                     maxLength={50}
                     onChange={(e) => setFavoriteDishes(e.target.value)}
-                    className="p-2 rounded text-black outline"
+                    className="p-2 w-full rounded text-black outline"
                     required
                 />
 
@@ -101,11 +102,11 @@ export const AddReview = () => {
                     value={comments}
                     maxLength={100}
                     onChange={(e) => setComments(e.target.value)}
-                    className="p-2 rounded text-black outline"
+                    className="p-2 w-full rounded text-black outline"
                     required
                 />
 
-                <button className="ml-auto shrink-0 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
+                <button className="w-full sm:col-span-2 lg:col-span-3 bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">
                     + Add Review
                 </button>
             </form>
